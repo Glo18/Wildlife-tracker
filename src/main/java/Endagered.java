806fe56ce221ff1758ab914b8b2aefae7f60f93b
Glo18 <kiogoraspecial@gmail.com>
2021-03-02 13:27:53 +0300

    use getter methods

--- src/main/java/Endagered.java
+++ src/main/java/Endagered.java
@@ -1,11 +1,36 @@
 public class Endagered {
+    private int id;
     private String name;
+    private String health;
 
     public Endagered(String name, String health, String age) {
+        this.id = id;
         this.name = name;
+        this.health = health;
     }
 
     public String getName() {
+
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHealth(){
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
