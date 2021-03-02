e19b1975aa45fc366ca89a769a36f0b6fbd6ce2a
Glo18 <kiogoraspecial@gmail.com>
2021-03-02 13:23:26 +0300

    add name properties

--- src/main/java/Endagered.java
+++ src/main/java/Endagered.java
@@ -1,2 +1,11 @@
 public class Endagered {
+    private String name;
+
+    public Endagered(String name, String health, String age) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/EndageredTest.java
+++ src/test/java/EndageredTest.java
@@ -1,5 +1,7 @@
 import org.junit.*;
 
+import static org.junit.Assert.assertEquals;
+
 public class EndageredTest {
     @Test
     public void Animals_instantiatesName_of_the_animal_true_String() {
