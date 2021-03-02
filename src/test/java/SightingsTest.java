57cef58da4caebf0b568ab9822e47002905c681a
Glo18 <kiogoraspecial@gmail.com>
2021-03-02 13:29:58 +0300

    create location test

--- src/test/java/SightingsTest.java
+++ src/test/java/SightingsTest.java
@@ -1,5 +1,10 @@
-import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-public class SightingsTest extends TestCase {
+public class SightingsTest {
 
-}
+    @Test
+    public void Sighting_location_true_string(){
+        Sightings testSightings = new Sightings("Zone B", "Kayle", "rhino");
+        assertEquals("Zone B", testSightings.getLocation());
+    }
