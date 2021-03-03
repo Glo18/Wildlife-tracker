c02e3fdef8a8a60be29db8deac41ddafa517fabb
Glo18 <kiogoraspecial@gmail.com>
2021-03-03 13:51:52 +0300

    add a post method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,5 +19,16 @@ public class App {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "AnimalsForm.hbs");
         }, new HandlebarsTemplateEngine());
+
+        post("/addAnimal", (request, response) -> {
+
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("animalsName");
+
+            Animals animals = new Animals(name);
+            animals.save();
+            model.put("animals", animals);
+            return new ModelAndView(model, "SuccessAnimal.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
