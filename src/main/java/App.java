1f86f892e6ddf9b534fa741ef71b6f0f71ef3f59
Glo18 <kiogoraspecial@gmail.com>
2021-03-03 13:54:57 +0300

    add a post and get method for endagered species

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -30,5 +30,28 @@ public class App {
             model.put("animals", animals);
             return new ModelAndView(model, "SuccessAnimal.hbs");
         }, new HandlebarsTemplateEngine());
+
+        get("/Animals", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("Animals", Animals.getAllAnimals());
+            return new ModelAndView(model, "Animals.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/EndageredForm", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "EndageredForm.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/report", (request, response) -> {
+
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("name");
+            String health = request.queryParams("health");
+            String age = request.queryParams("age");
+            Endagered endangered = new Endagered(name, health, age);
+            endangered.save();
+            model.put("endangered", endangered);
+            return new ModelAndView(model, "SuccessEndagered.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
