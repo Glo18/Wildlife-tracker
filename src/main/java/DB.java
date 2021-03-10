af6d44cc109af9b4343d439d4ded9aa62e44831e
Glo18 <kiogoraspecial@gmail.com>
2021-03-10 12:34:37 +0300

    add credentials

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,5 +2,5 @@ import org.sql2o.Sql2o;
 
 public class DB {
 //    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "glory", "myPassword");
-public static Sql2o sql2o=new Sql2o("jdbc:postgresql://ec2-54-162-119-125.compute-1.amazonaws.com:5432/de43vj0csammnd", "imxqkdlqepamqj","433eb96d656b66b08941597066d2147c2b43a30c4709c6a9ef67e2ab5ed9207b");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-160-7-200.compute-1.amazonaws.com:5432/d6gl5ars6a27pe", "kjkzbdehtaikdq", "870ab612052e47e42b835def4bacfd02c635a330d7883da8f2000b76a8f88b3c");
 }
